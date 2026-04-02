2891ffef0d30d3c6fa70f6fce6131ebf31979513
anastasiia <nasti2007neijmak@gmail.com>
2026-04-02 21:22:28 +0300

    fix

--- src/server.js
+++ src/server.js
@@ -6,14 +6,31 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const notes = express();
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-notes.use(cors());
-notes.use(pinoHttp());
-notes.use(express.json());
-notes.use(cors());
-notes.use(
+let logger;
+
+if (process.env.NODE_ENV === 'production') {
+  logger = pino();
+} else {
+  logger = pino({
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+      },
+    },
+  });
+}
+
+app.use(cors());
+app.use(pinoHttp( logger));
+app.use(express.json());
+app.use(cors());
+app.use(
   pino({
     level: 'info',
     transport: {
@@ -29,27 +46,38 @@ notes.use(
   }),
 );
 
-notes.get('/', (req, res) =>
+app.get('/notes', (req, res) =>
   res.status(200).json({ "message": "Retrieved all notes" })
 );
 
-notes.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-notes.get('/notes/:noteId', (req, res) => {
+app.get('/notes/:noteId', (req, res) => {
   const { noteId } = req.params;
   res.status(200).json({ noteId,
     "message": `Retrieved note with ID: ${noteId}`
   });
 });
 
-notes.use((req, res) => {
+app.get('/test-error', (req, res, next) => {
+  try {
+  throw new Error('Simulated server error');
+} catch (error) {
+  next(error);
+}
+});
+
+app.use((req, res) => {
   res.status(404).json({ message: 'Route not found' });
 });
 
-notes.get('/test-error', () => {
-  // eslint-disable-next-line no-undef
-  res.status(500).json({ message: 'Simulated server error' });
-  throw new Error('Simulated server error');
+app.use((err, req, res, next) => {
+  req.log.error(err);
+  res.status(500).json({ message: err.message || 'Internal server error' });
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
 });
