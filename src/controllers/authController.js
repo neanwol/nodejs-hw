95bc4612de275c30bb14f6be2cd8573ce3a4395d
anastasiia <nasti2007neijmak@gmail.com>
2026-04-15 10:37:44 +0300

    deploy fix

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,7 +4,7 @@ import createHttpError from 'http-errors';
 import { User } from '../models/user.js';
 import { Session } from "../models/session.js";
 import { createSession, setSessionCookies } from '../services/auth.js';
-import { sendEmail } from '../utils/sendEmail.js';
+import { sendEmail } from '../utils/sendMail.js';
 import handlebars from 'handlebars';
 import path from 'node:path';
 import fs from 'node:fs/promises';
